86453df5ca62cc2ade46e52448e6ea1f15af8c1b
Konstantin <klyschenko@ourgapps.com>
2025-10-24 18:08:46 +0400

    CreateNoteScreen

--- app/src/main/java/com/klyschenko/notes/presentation/screens/notes/NotesScreen.kt
+++ app/src/main/java/com/klyschenko/notes/presentation/screens/notes/NotesScreen.kt
@@ -1,6 +1,5 @@
 package com.klyschenko.notes.presentation.screens.notes
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.combinedClickable
